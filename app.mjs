9bf18fb857a16bb9ffaf013c31be42bf58acd49a
Jamir <Jamirong06@gmail.com>
2024-11-25 11:20:44 -0500

    updated

--- app.mjs
+++ app.mjs
@@ -1,23 +1,37 @@
 import { Sequelize, DataTypes } from "sequelize";
-const sequelize = new Sequelize('ecommerce', 'root', '4_P422w0rd!', {
-    host: 'localhost',
-    dialect: 'mysql'
-  });
 
-  const customer = sequelize.define('xcustomer', {
-    name: { type: DataTypes.STRING, allowNull: false },
-    customer_id: { type: DataTypes.INTEGER, allowNull: false },
-    city: { type: DataTypes.STRING, allowNull: true }
-  });
+// Grabs NAME, USER, and PASSWORD from env file to validate information
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,  
+  process.env.DB_PASSWORD,  
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT
+});
+
+// creates a table with the columns of NAME, CUSTOMER_ID, and CITY
+const customer = sequelize.define('xcustomer', {
+
+  name: { type: DataTypes.STRING, allowNull: false },
+  customer_id: { type: DataTypes.INTEGER, allowNull: false },
+  city: { type: DataTypes.STRING, allowNull: true }
+
+});
   
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-  
-  sequelize.sync({ force: true }).then(() => {
-    console.log('Database synced');
-  });
+//Attempts to authenticate a connection
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} 
+// When/If an error is caught, an error message will display
+catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+// When the databse is synced, tables will be recreated, and then logged
+sequelize.sync({ force: true }).then(() => {
+  console.log('Database synced');
+});
   
